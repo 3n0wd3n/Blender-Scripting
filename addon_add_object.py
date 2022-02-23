7d3c9d06fe454d969e45d55753e05a3c8b19b3ec
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-02-23 19:56:19 +0100

    Add files via upload

---
+++ addon_add_object.py
@@ -0,0 +1,26 @@
+import bpy
+
+class TestPanel(bpy.types.Panel):
+    bl_label = "MyCube"
+    bl_idname = "PT_TestPanel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "NewTab"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text= "Add my own cube", icon = "CUBE")
+        for i in range(2):
+            row = layout.row()
+        row.operator("mesh.primitive_cube_add")
+        
+    
+def register():
+    bpy.utils.register_class(TestPanel)
+def unregister():
+    bpy.utils.register_class(TestPanel)
+    
+if __name__ == "__main__":
+    register()
