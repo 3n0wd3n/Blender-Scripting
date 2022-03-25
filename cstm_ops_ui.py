b86885b6eeae66792fd6dc7b55e55f3281fce91d
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-03-25 07:52:39 +0100

    Update cstm_ops_ui.py

--- cstm_ops_ui.py
+++ cstm_ops_ui.py
@@ -1,1 +1,8 @@
 # 24.3.2022
+
+#SIMPLE properties
+name = bpy.props.StringProperty(name="Enter your name")
+age = bpy.props.IntStringProperty(name="Enter your age", min = 1, max = 100)
+alive = bpy.props.BoolProperty(name="Alive or not ?")
+
+#VECTOR properties
